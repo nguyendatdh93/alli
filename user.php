78fddfbc476800599086fe9345cbe6a66166f97b
DATNV <nguyendatdh93@gmail.com>
2017-07-22 08:18:07 -0700

    add tutorial
    add tutorial

--- user.php
+++ user.php
@@ -2,9 +2,23 @@
 	/**
 	 * Main code.
 	 */
-	require 'UserModel.php';
+	require 'usermodel.php';
+	require 'access.php';
 	$user = new UserModel();
-	// get method of api
+	/**
+	 * check if exits token key.
+	 * if token is not right. return json_error.
+	 */
+	if(!isset($_SERVER['HTTP_TOKEN'])){
+		echo json_encode('Api needs a token key to use');
+		return;
+	}else{
+		$access = new AccessModel($_SERVER['HTTP_TOKEN']);
+		if(!$access->check_token()){
+			echo json_encode('Invalid token key for API');
+			return;
+		}
+	}
 	$method = $_SERVER['REQUEST_METHOD'];
 	$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 	$email = array_shift($request);
